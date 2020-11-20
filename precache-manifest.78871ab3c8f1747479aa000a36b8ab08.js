2ca987199316f6843ed6d98cecc733f39a9fea7a
robert <runshacking@gmail.com>
2020-11-20 18:51:08 +0200

    Updates

---
+++ precache-manifest.78871ab3c8f1747479aa000a36b8ab08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f644eb1023d7361edeae9b59b9a4a1",
+    "url": "/ColorPicker/index.html"
+  },
+  {
+    "revision": "7528491a76fdff9300d3",
+    "url": "/ColorPicker/static/css/2.405d60bc.chunk.css"
+  },
+  {
+    "revision": "10a8ec017552394a1f6a",
+    "url": "/ColorPicker/static/css/main.6c44a5c9.chunk.css"
+  },
+  {
+    "revision": "7528491a76fdff9300d3",
+    "url": "/ColorPicker/static/js/2.9ec796e6.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/ColorPicker/static/js/2.9ec796e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10a8ec017552394a1f6a",
+    "url": "/ColorPicker/static/js/main.180cf4eb.chunk.js"
+  },
+  {
+    "revision": "79d42a2d156ae35aeff3",
+    "url": "/ColorPicker/static/js/runtime-main.af3ca29c.js"
+  }
+]);
